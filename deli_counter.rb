fc984db4a38ebb802efddf7fe47035cc65cc6a9d
Megan Gomes <megan.gomes25@gmail.com>
2020-04-02 23:37:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,8 @@ def line(katz_deli)
     
   else 
     string = "The line is currently: "
-    katz_deli.each.with_index(1) do |person , index|
-      string += "#{index}. #{person}"
+    katz_deli.each.with_index(1) do |person, index|
+      string += " #{index}. #{person}"
     end 
   puts string 
   end 
