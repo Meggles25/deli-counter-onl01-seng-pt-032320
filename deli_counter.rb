f9a559bfa6f53186e46b3add0e90d834fd74bbb6
Megan Gomes <megan.gomes25@gmail.com>
2020-04-02 23:34:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line(katz_deli)
      puts "The line is currently empty."
     
   else 
-    string = "The line is currently: "
+    string = "The line is currently:"
     katz_deli.each.with_index(1) do |person, index|
       string += "#{index}. #{person}"
     end 
